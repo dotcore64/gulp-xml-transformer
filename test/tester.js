44654bc589faf7cf8a46b90f3100188b42829c31
Julian Grinblat <julian@dotcore.co.il>
2016-06-14 15:04:04 +0900

    Refactor tests

--- test/tester.js
+++ test/tester.js
@@ -1,9 +1,5 @@
+import libxmljs from 'libxmljs';
 import { readTestFile } from './helper.js';
-import {
-  funcChangeText, funcChangeAttr, funcAddChild,
-  objChangeText, objChangeAttr, objAddAttr, objAddChangeAttr,
-  arrMultiChange, nsUri, nsDefault, nsA,
-} from './transformations.js';
 
 const expectedText = readTestFile('test.text.xml');
 const expectedAttr = readTestFile('test.attr.xml');
@@ -11,55 +7,141 @@ const expectedChild = readTestFile('test.child.xml');
 const expectedAddAttr = readTestFile('test.add_attr.xml');
 const expectedAddChangeAttr = readTestFile('test.add_change_attr.xml');
 const expectedMulti = readTestFile('test.multi.xml');
-
 const expectedNs = readTestFile('namespaced.expected.xml');
 
+const nsUri = 'https://github.com/perrin4869/gulp-xml-transformer';
+
 export default (tester, namespacedTester) => {
+  const defineTest = ({ description, expected, transformation }) => {
+    it(description, done => {
+      tester(transformation, expected, done);
+    });
+  };
+
+  const defineNamespacedTest = ({ description, expected, namespaces, transformation }) => {
+    it(description, done => {
+      namespacedTester(transformation, expected, namespaces, done);
+    });
+  };
+
   describe('functions', () => {
-    it('should change name tag with function', done => {
-      tester(funcChangeText, expectedText, done);
-    });
+    [
+      {
+        description: 'should change name tag with function',
+        expected: expectedText,
+        transformation(xml) {
+          xml.get('//name').text('new name');
+          return xml;
+        },
+      },
+      {
+        description: 'should change attr value with function',
+        expected: expectedAttr,
+        transformation(xml) {
+          xml.get('//version').attr({ major: '10' });
+          return xml;
+        },
+      },
+      {
+        description: 'should add child node with function',
+        expected: expectedChild,
+        transformation(xml) {
+          xml.get('//name').text('new name');
 
-    it('should change attr value with function', done => {
-      tester(funcChangeAttr, expectedAttr, done);
-    });
+          const child = new libxmljs.Element(xml, 'child');
+          child.text('child element');
+          xml.get('//name').addChild(child);
 
-    it('should add child node with function', done => {
-      tester(funcAddChild, expectedChild, done);
-    });
+          return xml;
+        },
+      },
+    ].forEach(defineTest);
   });
 
   describe('objects', () => {
-    it('should change name tag with object', done => {
-      tester(objChangeText, expectedText, done);
-    });
-
-    it('should change attr value with object', done => {
-      tester(objChangeAttr, expectedAttr, done);
-    });
-
-    it('should add attr value with object', done => {
-      tester(objAddAttr, expectedAddAttr, done);
-    });
-
-    it('should add and change attr values with object', done => {
-      tester(objAddChangeAttr, expectedAddChangeAttr, done);
-    });
+    [
+      {
+        description: 'should change name tag with object',
+        expected: expectedText,
+        transformation: {
+          path: '//name',
+          text: 'new name',
+        },
+      },
+      {
+        description: 'should change attr value with object',
+        expected: expectedAttr,
+        transformation: {
+          path: '//version',
+          attr: { major: 10 },
+        },
+      },
+      {
+        description: 'should add attr value with object',
+        expected: expectedAddAttr,
+        transformation: {
+          path: '//version',
+          attr: { build: 20 },
+        },
+      },
+      {
+        description: 'should add and change attr values with object',
+        expected: expectedAddChangeAttr,
+        transformation: {
+          path: '//version',
+          attrs: [
+            { major: 10 },
+            { minor: 11 },
+            { build: 20 },
+          ],
+        },
+      },
+    ].forEach(defineTest);
   });
 
   describe('arrays', () => {
-    it('should change name tag with array', done => {
-      tester(arrMultiChange, expectedMulti, done);
-    });
+    [
+      {
+        description: 'should change name tag with array',
+        expected: expectedMulti,
+        transformation: [
+          {
+            path: '//version',
+            attrs: [
+              { major: 10 },
+              { minor: 11 },
+              { build: 20 },
+            ],
+          },
+          {
+            path: '//name',
+            text: 'new name',
+          },
+        ],
+      },
+    ].forEach(defineTest);
   });
 
   describe('namespaced', () => {
-    it('should change name tag within default ns', done => {
-      namespacedTester(nsDefault, expectedNs, nsUri, done);
-    });
-
-    it('should change name tag within a ns', done => {
-      namespacedTester(nsA, expectedNs, { a: nsUri }, done);
-    });
+    [
+      {
+        description: 'should change name tag within default ns',
+        expected: expectedNs,
+        namespaces: nsUri,
+        transformation: {
+          path: '//xmlns:name',
+          text: 'new name',
+        },
+      },
+      {
+        description: 'should change name tag within a ns',
+        expected: expectedNs,
+        namespaces: { a: nsUri },
+        transformation: {
+          path: '//a:name',
+          text: 'new name',
+        },
+      },
+    ].forEach(defineNamespacedTest);
   });
 };
