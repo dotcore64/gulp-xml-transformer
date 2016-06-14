008a6a8ce9308060303fea31e90943216b525712
Julian Grinblat <julian@dotcore.co.il>
2016-06-14 13:47:25 +0900

    Group different tests

--- test/tester.js
+++ test/tester.js
@@ -13,35 +13,41 @@ const expectedAddChangeAttr = readTestFile('test.add_change_attr.xml');
 const expectedMulti = readTestFile('test.multi.xml');
 
 export default (tester) => {
-  it('should change name tag with function', done => {
-    tester(funcChangeText, expectedText, done);
+  describe('functions', () => {
+    it('should change name tag with function', done => {
+      tester(funcChangeText, expectedText, done);
+    });
+
+    it('should change attr value with function', done => {
+      tester(funcChangeAttr, expectedAttr, done);
+    });
+
+    it('should add child node with function', done => {
+      tester(funcAddChild, expectedChild, done);
+    });
   });
 
-  it('should change attr value with function', done => {
-    tester(funcChangeAttr, expectedAttr, done);
+  describe('objects', () => {
+    it('should change name tag with object', done => {
+      tester(objChangeText, expectedText, done);
+    });
+
+    it('should change attr value with object', done => {
+      tester(objChangeAttr, expectedAttr, done);
+    });
+
+    it('should add attr value with object', done => {
+      tester(objAddAttr, expectedAddAttr, done);
+    });
+
+    it('should add and change attr values with object', done => {
+      tester(objAddChangeAttr, expectedAddChangeAttr, done);
+    });
   });
 
-  it('should add child node with function', done => {
-    tester(funcAddChild, expectedChild, done);
-  });
-
-  it('should change name tag with object', done => {
-    tester(objChangeText, expectedText, done);
-  });
-
-  it('should change attr value with object', done => {
-    tester(objChangeAttr, expectedAttr, done);
-  });
-
-  it('should add attr value with object', done => {
-    tester(objAddAttr, expectedAddAttr, done);
-  });
-
-  it('should add and change attr values with object', done => {
-    tester(objAddChangeAttr, expectedAddChangeAttr, done);
-  });
-
-  it('should change name tag with array', done => {
-    tester(arrMultiChange, expectedMulti, done);
+  describe('arrays', () => {
+    it('should change name tag with array', done => {
+      tester(arrMultiChange, expectedMulti, done);
+    });
   });
 };
