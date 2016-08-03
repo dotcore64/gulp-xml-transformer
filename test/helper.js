f8f7e91942c5da06f3d835aaa77a899d9192d9d2
Julian Grinblat <julian@dotcore.co.il>
2016-08-03 14:10:46 +0900

    Upgrade dependencies

--- test/helper.js
+++ test/helper.js
@@ -1,4 +1,5 @@
 import path from 'path';
 import { readFileSync } from 'fs';
 
-export const readTestFile = filePath => readFileSync(path.join(__dirname, filePath), 'utf8');
+export const readTestFile = // eslint-disable-line import/prefer-default-export
+  filePath => readFileSync(path.join(__dirname, filePath), 'utf8');
