ec937177046068ad40d6e223ed22bfd905c09088
Julian Grinblat <julian@dotcore.co.il>
2017-06-22 16:01:06 +0900

    Replace babel-core with babel-register

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const util = require('gulp-util');
 const babel = require('gulp-babel');
 const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
-const compiler = require('babel-core/register');
 
 const src = 'src/*.js';
 
