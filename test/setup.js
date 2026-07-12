1d4447d9c17dfd1721110d5aaf2a29e52355dfd2
Julian Grinblat <julian@dotcore.co.il>
2026-07-13 03:13:30 +0900

    chore!: replace mocha with node:test

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,9 @@
+import { describe, it } from "node:test";
+
 import { use } from "chai";
+
+globalThis.describe = describe;
+globalThis.it = it;
 
 use((await import("sinon-chai")).default); // eslint-disable-line unicorn/no-await-expression-member
 use((await import("chai-as-promised")).default); // eslint-disable-line unicorn/no-await-expression-member
